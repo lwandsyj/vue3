df9b6f0af77723638407150635556de9f8e2d6e8
li <lwaisyj@163.com>
2020-12-31 15:28:31 +0800

    vue3

---
+++ example/hello/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <button @[eventName]="handleClick">确定</button>
+    <button v-on:click="handleClick">确定</button>
+    <hello-world v-if="flag"></hello-world>
+    <div :[attrName]="'id'+id">
+
+    </div>
+    <p :id="`id${id}`"></p>
+    <div class="test" :class="{'isActive':isActive}" @click.capture="handleClick2">
+       <button @[eventName]="handleClick1">确定1</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  data() {
+    return {
+      flag: false,
+      id:1,
+      eventName:"click",
+      attrName:"data-id",
+      isActive:false
+    };
+  },
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    handleClick() {
+      this.flag = !this.flag;
+    },
+    handleClick1(){
+      console.log('click1')
+    },
+    handleClick2(){
+      this.isActive=!this.isActive
+      }
+  },
+  mounted() {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.test{
+  border:1px solid #d2d2d2;height:40px
+}
+.isActive{
+  border-color:red;
+}
+</style>
---
+++ example/hello/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted(){
+    console.log("hello component mounted")
+  },
+   // v3 error  The `destroyed` lifecycle hook is deprecated. Use `unmounted` instead 
+  unmounted(){
+    console.log("hello component dispose")
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
